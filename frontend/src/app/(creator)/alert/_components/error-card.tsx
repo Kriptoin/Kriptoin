36315d6ba9b73138d09af5f6bd84ed23d5f90820
cvpfus <47532266+yusufpraditya@users.noreply.github.com>
2025-04-26 13:28:53 +0700

    feat: add error card

---
+++ frontend/src/app/(creator)/alert/_components/error-card.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Loading from "@/components/ui/loading";
+
+export const ErrorCard = ({ title }: { title: string }) => {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardFooter className="flex-grow">
+        <p>An error occurred while loading the data. Please try again later.</p>
+      </CardFooter>
+    </Card>
+  );
+};
